59ede96bc5aef9b4b5465aa2c7c73fa6afb57bbc
Ananyaagupta <sonyguptashikohabad@gmail.com>
2024-02-21 22:34:27 +0530

    GET Method

---
+++ Employee/src/main/java/com/example/Employee/model/Employees.java
@@ -0,0 +1,27 @@
+package com.example.Employee.model;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int empId;
+    @Column(name = "name")
+    private String empName;
+    @Column(name = "salary")
+    private long empSalary;
+    @Column(name = "address")
+    private String empAddress;
+
+}
