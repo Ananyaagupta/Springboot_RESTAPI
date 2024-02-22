d765c47e23f07a902beca72867114c0274f08add
Ananyaagupta <sonyguptashikohabad@gmail.com>
2024-02-22 20:55:18 +0530

    merged all controllers

--- Employee/src/main/java/com/example/Employee/controller/EmployeeController.java
+++ Employee/src/main/java/com/example/Employee/controller/EmployeeController.java
@@ -1,23 +1,42 @@
 package com.example.Employee.controller;
 
-
 import com.example.Employee.model.Employees;
 import com.example.Employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/employees/fetch")
+@RequestMapping("/company/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping
+    @GetMapping("/fetch")
     public List<Employees> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployeeById(@PathVariable int id) {
+        if(employeeService.deleteThisEmployees(id)) {
+            return ResponseEntity.ok("Employee with ID " + id + " deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No employee found with ID " + id);
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addEmployee(@RequestParam Integer id, @RequestParam String name, @RequestParam long salary, @RequestParam String address) {
+        if (employeeService.addEmployee(id,name,salary,address)) {
+            return ResponseEntity.ok("Employee with ID " + id + " added successfully");
+
+        }
+        return ResponseEntity.status(HttpStatusCode.valueOf(77)).body("Could not add");
+    }
+
 }
--- Employee/src/main/java/com/example/Employee/service/EmployeeService.java
+++ Employee/src/main/java/com/example/Employee/service/EmployeeService.java
@@ -4,8 +4,6 @@ import com.example.Employee.model.Employees;
 import com.example.Employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
